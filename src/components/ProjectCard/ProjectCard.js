0345dd1d0eb9da09d5140794cd9632c40f7f024f
Amy Rippeto <amyrippeto@gmail.com>
2019-12-04 17:35:57 -0700

    Append projects and palettes

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -2,10 +2,41 @@ import React from 'react';
 import './ProjectCard.scss';
 
 const ProjectCard = ({ project }) => {
-
+  const { palettes } = project;
+  const paletteCards = palettes.map(palette => {
+    return <section 
+      key={palette.id}
+      className='palette'
+      >
+      <h4>{palette.name}</h4>
+      <div className='five-colors'>
+        <div 
+          className='palette-color'
+          style={{backgroundColor: `${palette.color1}`}}
+        ></div>
+        <div 
+          className='palette-color'
+          style={{backgroundColor: `${palette.color2}`}}
+        ></div>
+        <div 
+          className='palette-color'
+          style={{backgroundColor: `${palette.color3}`}}
+        ></div>
+        <div 
+          className='palette-color'
+          style={{backgroundColor: `${palette.color4}`}}
+        ></div>
+        <div 
+          className='palette-color'
+          style={{backgroundColor: `${palette.color5}`}}
+        ></div>
+      </div>
+    </section>
+  })
   return (
     <article>
-      <h2>Project Card</h2>
+      <h2>{project.name}</h2>
+      { paletteCards }
     </article>
   )
 
--- src/components/ProjectsContainer/ProjectsContainer.js
+++ src/components/ProjectsContainer/ProjectsContainer.js
@@ -3,11 +3,13 @@ import ProjectCard from '../ProjectCard/ProjectCard';
 import './ProjectsContainer.scss';
 
 const ProjectsContainer = ({ projects }) => {
-
+  const projCards = projects.map(project => {
+    return <ProjectCard key={project.id} project={project}/>
+  })
   return (
     <section>
       <h2>Project Container</h2>
-      <ProjectCard />
+      { projCards }
     </section>
   )
 
