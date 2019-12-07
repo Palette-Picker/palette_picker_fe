bbb7292ba0643ccd84a406c2b7e6c0933a7bebb4
Amy Rippeto <amyrippeto@gmail.com>
2019-12-06 19:42:27 -0700

    Add fetch call for adding a new palette

--- src/utils/apiCalls.js
+++ src/utils/apiCalls.js
@@ -24,7 +24,31 @@ export const addProject = async (newProject) => {
   if (!response.ok) {
     throw Error('Unable to add project.')
   };
-  const addedProject = await response.json()
-  console.log('added in apiCalls', addedProject)
+  const addedProject = await response.json();
+  return addedProject;
+};
+
+export const addPalette = async (newPalette) => {
+  const options = {
+    method: 'POST', 
+    body: JSON.stringify({
+      name: newPalette.name,
+      color1: newPalette.color1,
+      color2: newPalette.color2,
+      color3: newPalette.color3,
+      color4: newPalette.color4,
+      color5: newPalette.color5,
+      project_id: newPalette.project_id
+    }),
+    headers: {
+      'content-type': 'application/json'
+    }
+  };
+    const response = await fetch(`${baseUrl}/palettes`, options);
+    if (!response.ok) {
+      throw Error('Unable to add palette')
+    };
+    const addedPalette = await response.json();
+    return addedPalette;
 };
 
