456aa72fc57bb5bc1e32bcc8bb538b385b8e2a5f
Amy Rippeto <amyrippeto@gmail.com>
2019-12-11 22:49:07 -0700

    Style HeaderComponent

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,8 +7,8 @@ const Header = () => {
   return (
     <header>
       <h1>Palette Picker</h1>
-      <NavLink to='/'>Home</NavLink>
-      <NavLink to='/projects'>Projects</NavLink>
+      <NavLink exact activeClassName="active" className="nav-link" to='/'>Home</NavLink>
+      <NavLink activeClassName="active" className="nav-link" to='/projects'>Projects</NavLink>
     </header>
 
   )
